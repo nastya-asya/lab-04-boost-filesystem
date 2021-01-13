8cdf6b7cc2147ba01f4a7ee9fa267fa222fdc936
nastya-asya <61165064+nastya-asya@users.noreply.github.com>
2021-01-13 22:33:20 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,10 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Anastasiya Smirnova nastya.asya08@yandex.ru
 
 #include <gtest/gtest.h>
+#include <header.hpp>
 
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+  std::string ftp_path = "../";
+  fillup(ftp_path);
+  EXPECT_EQ(brokers.size(), 3);
 }
